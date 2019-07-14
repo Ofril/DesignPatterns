f3b0cea51fb853c8610944ed10e9b49d429bb174
Ofril <37039639+Ofril@users.noreply.github.com>
2019-07-14 14:13:46 +0300

    Ex2 - Factory

---
+++ FactoryMethod/Saguie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Saguie : Vehicle
+    {
+        protected string wheelSize;
+
+        public Saguie(string model, float cylinder, string color, int numberOfWheels, string wheelSize) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.wheelSize = wheelSize;
+        }
+
+        public override string ToString()
+        {
+            return "Saguie " + base.ToString() + $" {wheelSize}";
+        }
+    }
+}
---
+++ FactoryMethod/Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Track : Vehicle
+    {
+        protected string lightColor;
+
+        public Track(string model, float cylinder, string color, int numberOfWheels, string lightColor) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.lightColor = lightColor;
+        }
+
+        public override string ToString()
+        {
+            return "Track " + base.ToString() + $" {lightColor}";
+        }
+    }
+}
