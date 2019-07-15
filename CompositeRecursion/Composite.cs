211377484e03ac740a1464d5cb78a4c4ccd99f10
Ofril <37039639+Ofril@users.noreply.github.com>
2019-07-15 14:14:01 +0300

    Ex8 - CompositeRecursion

---
+++ CompositeRecursion/Composite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Composite : Component
+    {
+
+        IList<Component> childs = new List<Component>();
+
+        public Composite(string name) : base(name)
+        {
+        }
+
+        public override void AddChild(Component c)
+        {
+            childs.Add(c);
+        }
+
+        public override IList<Component> GetChilds()
+        {
+            return childs;
+        }
+
+        public override void RemoveChild(Component c)
+        {
+            childs.Remove(c);
+        }
+
+        public override void Draw(string space)
+        {
+            Console.WriteLine(space + this.name);
+        }
+
+        public override bool IsBinary()
+        {
+            return GetChilds().Count() == 0 || GetChilds().Count() == 2;
+        }
+    }
+}
