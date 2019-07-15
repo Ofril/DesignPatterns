a3f700c53586a01f480c62f7f75a0dc790bc1ae2
Ofril <37039639+Ofril@users.noreply.github.com>
2019-07-15 16:37:42 +0300

    Ex10 - PrototypeWebInfo

---
+++ PrototypeWebInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeWebInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://www.IntelliTect.com";
+            string url2 = "http://www.IntelliTect.com";
+            string url3 = "http://www.Blabla.com";
+            WebInfoFactory webInfoFactory = new WebInfoFactory();
+
+            Console.WriteLine(url);
+            Console.WriteLine(webInfoFactory.CreateWebInfo(url).ToString());
+            Console.WriteLine(url2);
+            Console.WriteLine(webInfoFactory.CreateWebInfo(url2).ToString());
+            Console.WriteLine(url3);
+            Console.WriteLine(webInfoFactory.CreateWebInfo(url3).ToString());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ PrototypeWebInfo/WebInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeWebInfo
+{
+    class WebInfo : IProtoType<WebInfo>
+    {
+        DateTime timestamp;
+        string size;
+        string url;
+
+        public WebInfo(DateTime timestamp, string size, string url)
+        {
+            this.timestamp = timestamp;
+            this.size = size;
+            this.url = url;
+        }
+
+        public WebInfo clone()
+        {
+            return new WebInfo(timestamp, size, url);
+        }
+
+        public string ToString()
+        {
+            return $"Timestamp: {timestamp} Size: {size} Url: {url} "; 
+        }
+    }
+}
