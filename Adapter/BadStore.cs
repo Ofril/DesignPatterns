44bb174cce9fcae79c3c1f025e8f7fc10769e1c0
Ofril <37039639+Ofril@users.noreply.github.com>
2019-07-16 11:22:33 +0300

    Ex12- Adapter

---
+++ Adapter/BadStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class BadStore : IDBWebData
+    {
+
+        public void store(WebClientData webClientData)
+        {
+            Console.WriteLine("Web client data is not so nice ... " + webClientData.ToString());
+
+        }
+    }
+
+}
---
+++ Adapter/ClientData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class ClientData
+    {
+        public string Name;
+        public int ID;
+
+        public ClientData(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+
+        public string ToString()
+        {
+            return $"name: {Name} ID: {ID}";
+        }
+    }
+}
