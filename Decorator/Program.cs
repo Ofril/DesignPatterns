3ad8b4475a5c0d3d827e90b81a3bd74fc380afd4
Ofril <37039639+Ofril@users.noreply.github.com>
2019-07-17 09:49:31 +0300

    Ex15 - Decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPizza pizza = new BasePizza();
+
+            IPizza pizzaWithTomato = new TomatoTopping(pizza);
+            IPizza pizzaWithTomatoAndPineapple = new PineaappleTopping(pizzaWithTomato);
+            IPizza pizzaWithTomatoAndPineappleAndCorn = new CornTopping(pizzaWithTomatoAndPineapple);
+            IPizza pizzaWithTomatoAndPineappleAndCornAndHalf = new HalfPriceTopping(pizzaWithTomatoAndPineappleAndCorn);
+
+            Console.WriteLine(pizzaWithTomatoAndPineappleAndCornAndHalf.GetDetails());
+            Console.WriteLine(pizzaWithTomatoAndPineappleAndCornAndHalf.GetPrice());
+
+            IPizza myfavoritePizza = new TomatoTopping(new CheeseTopping(new BasePizza()));
+
+        }
+    }
+}
