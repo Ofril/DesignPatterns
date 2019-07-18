b7af8add9c4503cd201bb665ee55cd74eb39bad9
Ofril <37039639+Ofril@users.noreply.github.com>
2019-07-18 09:42:11 +0300

    Ex20 - Strategy

---
+++ Strategy/NumberCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class NumberCollection
+    {
+        protected List<int> numbers = new List<int>();
+        protected ISort sorter;
+        private const int NUMBER = 5;
+
+        public NumberCollection(ISort sorter)
+        {
+            this.sorter = sorter;
+        }
+
+        public void Add(int num)
+        {
+            this.numbers.Add(num);
+
+            if (this.numbers.Count > NUMBER)
+            {
+                sorter = new QuickSort();
+            }
+
+            Sort();
+        }
+
+        public void Remove(int num)
+        {
+            this.numbers.Remove(num);
+
+            if (this.numbers.Count <= NUMBER)
+            {
+                sorter = new MergeSort();
+            }
+
+            Sort();
+        }
+         
+        public void Sort()
+        {
+            sorter.Sort(numbers);
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberCollection collection = new NumberCollection(new MergeSort());
+            collection.Add(1);
+            collection.Add(5);
+            collection.Add(4);
+            collection.Add(3);
+            collection.Add(15);
+            collection.Add(12);
+            collection.Add(7);
+            collection.Remove(7);
+            collection.Remove(1);
+            collection.Remove(5);
+        }
+    }
+}
